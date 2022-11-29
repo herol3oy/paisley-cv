82fe122cfd82173a2ee8517a57c8f4257b2ca10a
Hamed Sedighi <hamed.sedighi@eyplsdc.onmicrosoft.com>
2022-11-29 23:15:33 +0100

    add ng es lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'paisley-cv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
